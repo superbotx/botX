7cf259e341e594fca5e91fdc23624c50c1ade3b7
tianhaoz95 <tianhaoz@umich.edu>
2018-03-12 23:00:31 -0700

    update

--- botX/utils/install_util.py
+++ botX/utils/install_util.py
@@ -296,14 +296,12 @@ def catkin_make(path):
     if os_name != 'Linux':
         print(os_name + ' is not supported', 'Use a Linux machine (Ubuntu 16.04 suggested)')
         return
-    wd = os.getcwd()
-    os.chdir(path)
+    #wd = os.getcwd()
+    #os.chdir(path)
     print('starting catkin_make ...')
-    subprocess.call(['ls'])
-    subprocess.call(['catkin_make'])
-    subprocess.call(['pwd'])
+    subprocess.call(['catkin_make', '--directory', path])
     print('building finished')
-    os.chdir(wd)
+    #os.chdir(wd)
 
 def create_project(payload):
     project_name = process_create_payload(payload)
