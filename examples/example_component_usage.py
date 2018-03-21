b04dffcf9f3ea39d356e9264c5b815d2d4382412
tianhaoz95 <tianhaoz@umich.edu>
2018-03-21 14:11:17 -0700

    update

--- examples/example_component_usage.py
+++ examples/example_component_usage.py
@@ -1,10 +1,36 @@
 import time
+"""
+If you are using a botX module writen by other
+developer, then the following two lines will be
+changed to:
+
+from botX.applications import botXimport
+
+zed_camera = botXimport('zed_camera')
+"""
 from example_botX_export import botXexport
 
+"""
+When importing your own module or using botXimport, what
+you import is a dictionary consisting of all the modules
+in that package, and here is how you are going to use them
+"""
 zed_camera = botXexport['zed_camera']['module']()
 
+"""
+This is only for demo purpose, in normal situation this
+will be handled by the interface object.
+"""
 zed_camera.setup()
 
+"""
+This is also for demo purpose, to prevent the program from
+exiting immediately
+"""
 time.sleep(10)
 
+"""
+This is only for demo purpose, in normal situation this
+will be handled by the interface object.
+"""
 zed_camera.shutdown()
