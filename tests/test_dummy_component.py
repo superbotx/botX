b69f27ea499e7fea9795cfbf674b1e9580ab50ed
Tianhao Zhou <tianhaoz@umich.edu>
2018-04-10 18:05:44 -0700

    update

--- tests/test_dummy_component.py
+++ tests/test_dummy_component.py
@@ -16,8 +16,12 @@ class DummyComponent(BaseComponent):
     def get_img(self, config):
         return 'img'
 
+    def get_coord(self, bbox):
+        return 10
+
 def test_dummy_component():
     dummy = DummyComponent()
     assert(dummy.setup() == 3)
     assert(dummy.shutdown() == 5)
     assert(dummy.get_img('i want img') == 'img')
+    assert(dummy.get_coord('bbox') == 10)
