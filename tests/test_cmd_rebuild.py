3ffb16731bf65bc5c4a84a67ac3107aaaca14cb5
Tianhao Zhou <tianhaoz@umich.edu>
2018-04-15 21:28:00 -0700

    add cov

--- tests/test_cmd_rebuild.py
+++ tests/test_cmd_rebuild.py
@@ -11,7 +11,6 @@ def test_commands():
     exec_command(['botX', 'create', 'test_proj'])
     os.chdir('test_proj')
     exec_command(['botX', 'add', 'external', 'https://github.com/ros-planning/moveit_msgs.git'])
-    shutil.rmtree('external_modules/src/moveit_msgs')
-    exec_command(['botX', 'install'])
+    exec_command(['botX', 'rebuild'])
     os.chdir('..')
     shutil.rmtree('test_proj')
