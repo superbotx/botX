46defc5ae624aeb32176d73310a839708e46021c
Tianhao Zhou <tianhaoz@umich.edu>
2018-04-16 00:21:55 -0700

    add cov

--- tests/test_external_command.py
+++ tests/test_external_command.py
@@ -1,5 +1,12 @@
 from .botX.applications import external_command_pool
+import time
 
 def test_trivial_command():
     command = 'ls -A'
     external_command_pool.start_command(command)
+
+def test_ros_command():
+    command = 'roscore'
+    proc_id = external_command_pool.start_command(command)
+    time.sleep(1)
+    external_command_pool.end_command(proc_id)
