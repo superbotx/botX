e4a616e6007f20ccb94d944209124c4ba785ab15
vivianca <vivianca@stanford.edu>
2018-04-25 11:57:06 -0700

    Create test_exception_util.py

---
+++ tests/test_exception_util.py
@@ -0,0 +1,9 @@
+def test_error {
+	oops = CreateProjectError(ValueError)
+	error = "Your input value is invalid."
+	fix = "Enter a value between 0.2 and 0.9."
+
+	oops.__init__(self, error, fix)
+
+	return error = oops.err_msg, fix = oops.fix_msg:
+}
