384320079e46a63cc28e08e0b0a5b81354a98707
Vivian <vivianca@stanford.edu>
2018-04-28 15:55:18 -0700

    yay it's a different problem now

--- tests/test_exception_util.py
+++ tests/test_exception_util.py
@@ -1,10 +1,8 @@
 from .botX.utils.exception_util import CreateProjectError
 
 def test_error():
-	oops = CreateProjectError(ValueError)
-	error = "Your input value is invalid."
-	fix = "Enter a value between 0.2 and 0.9."
-
-	oops.__init__(self, error, fix)
+	oops = CreateProjectError("Project invalid.", "Fix project.")
+	error = "Project invalid."
+	fix = "Fix project."
 
 	return error == oops.err_msg and fix == oops.fix_msg
